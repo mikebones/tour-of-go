b716a479730ee47bc1f2780284e8fe10cccb5c2b
Michael Costas <micostas@amazon.com>
2022-07-28 02:52:34 +0000

    init io

--- leetcode/find_emails/main.go
+++ leetcode/find_emails/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io/fs"
@@ -42,5 +43,33 @@ func check(e error) {
 } 
 
 func main() {
-	findEmail("test.txt")
+	scanEmails("test.txt")
+}
+
+
+func findEmails(path string) {
+	_, err := os.Open(path)
+	check(err)
+	regex := regexp.MustCompile("[A-z0-9._]+@[A-z0-9.]+") // must compile handles err
+	entireFile, err := os.ReadFile(path)
+	check(err)
+	fmt.Printf("%q\n", regex.FindAll(entireFile, -1))
+}
+// scanner to do new lines?
+
+func scanEmails(path string) {
+	//open file
+	//remember to close file
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+	// compile regex
+	regexEmail := regexp.MustCompile("[A-z0-9._]+@[A-z0-9._]")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		regexEmail.FindString(scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
 }
